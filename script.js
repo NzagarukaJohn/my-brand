eee66dd5ec9c0ecef2aa78b9df5f35137e11e7c3
Nzagaruka John <nzagarukajohn31@gmail.com>
2022-02-19 10:49:09 +0200

    Implimanted fronted

--- script.js
+++ script.js
@@ -1,91 +1,67 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+const form = document.getElementById("contact-form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const userMsg = document.getElementById("message");
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+const errorMsg = document.getElementsByClassName("error");
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  validate(username, 0, "Username cannot be blank");
+  validate(email, 1, "Please enter a valid email address");
+  validate(subject, 2, "Subject cannot be blank");
+  validate(userMsg, 3, "Message cannot be blank");
 
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+     sendQuery();
 });
 
- // FORM VALIDATION
+let validate = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+  }
+};
 
+function sendQuery(){
+  let name = document.querySelector("#username").value,
+   email = document.querySelector("#email").value,
+   subject = document.querySelector("#subject").value,
+   message = document.querySelector("#message").value
 
- const form = document.getElementById('form');
- const username = document.getElementById('username');
- const email = document.getElementById('email');
- const subject = document.getElementById('subject');
- const userMsg = document.getElementById('message');
+    let newQuery = {
+       username:name,
+       email: email,
+       subject:subject,
+       message: message
+   }
+  fetch("https://john-my-brand.herokuapp.com/query",{
+    method: 'POST',
+    headers:{
+      "Accept":"application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newQuery)
+  })
+  .then(res=>{
+         if (res.ok == true) {
+            console.log(res)
+         }
+         else console.log(`Error Happened: ${res.status}`)
+  })
+   alert("Your query has been sent successfully");
+  // window.location = 'dashboard.html'
 
- const errorMsg = document.getElementsByClassName('error');
+}
 
- form.addEventListener("submit", (e) => {
-     e.preventDefault();
+var burgerMenu = document.getElementById("burger-menu");
+var overlay = document.getElementById("menu");
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("active");
+});
 
-     validate(username, 0, "Username cannot be blank");
-     validate(email, 1, "Please enter a valid email address");
-     validate(subject, 2, "Subject cannot be blank");
-     validate(userMsg, 3, "Message cannot be blank");
- });
-
- let validate = (id, serial, message) => {
-    if (id.value.trim() === "") {
-        errorMsg[serial].innerHTML = message;
-        id.style.border = "2px solid red";
-      } 
-      
-      else {
-        errorMsg[serial].innerHTML = "";
-        id.style.border = "2px solid green";
-      }
- }
